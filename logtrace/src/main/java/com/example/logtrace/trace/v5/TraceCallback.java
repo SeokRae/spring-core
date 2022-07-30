cbf718b26debcf2e7d8cbd16b7ff4f3aabd24fcd
seokrae <kslbsh@gmail.com>
2022-07-31 01:05:32 +0900

    Revert "feat: 로그 추적기 v5 - 전략 패턴 학습"
    This reverts commit 3ef5a5337afc06534a8281406aa21cbbfed21c46.

--- logtrace/src/main/java/com/example/logtrace/trace/v5/TraceCallback.java
+++
@@ -1,5 +0,0 @@
-package com.example.logtrace.trace.v5;
-
-public interface TraceCallback<T> {
-    T call();
-}
--- logtrace/src/test/java/com/example/logtrace/trace/v5/TemplateMethodTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.logtrace.trace.v5;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class TemplateMethodTest {
-
-}
